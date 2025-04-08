27bf60a5b41204d227deb2bd487ae1c43241155a
mLastovsky <LastovskyTech@gmail.com>
2025-04-09 02:22:26 +0300

    feat: enabled jpa auditing

--- src/main/java/com/mlastovsky/todoapp/SpringTodoAppApplication.java
+++ src/main/java/com/mlastovsky/todoapp/SpringTodoAppApplication.java
@@ -2,8 +2,10 @@ package com.mlastovsky.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringTodoAppApplication {
 
     public static void main(String[] args) {
