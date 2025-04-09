72c5d20d032aa8c5ae358d5568aba80f1d7361fd
mLastovsky <LastovskyTech@gmail.com>
2025-04-10 01:49:26 +0300

    feat: update todo dto's

--- src/main/java/com/mlastovsky/todoapp/dto/TodoRequestDto.java
+++ src/main/java/com/mlastovsky/todoapp/dto/TodoRequestDto.java
@@ -1,5 +1,6 @@
 package com.mlastovsky.todoapp.dto;
 
+import com.mlastovsky.todoapp.model.TodoStatus;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -7,6 +8,10 @@ public record TodoRequestDto(
 
         @NotBlank(message = "description should not be blank")
         String description,
+
+        @NotNull(message = "status is required")
+        @NotBlank(message = "status should be present")
+        TodoStatus status,
 
         @NotNull(message = "ownerId is mandatory")
         Long ownerId
--- src/main/java/com/mlastovsky/todoapp/dto/TodoResponseDto.java
+++ src/main/java/com/mlastovsky/todoapp/dto/TodoResponseDto.java
@@ -1,4 +1,16 @@
 package com.mlastovsky.todoapp.dto;
 
-public record TodoResponseDto() {
+import com.mlastovsky.todoapp.model.TodoStatus;
+
+import java.time.LocalDateTime;
+
+public record TodoResponseDto(
+
+        Long id,
+        String description,
+        TodoStatus status,
+        Long ownerId,
+        LocalDateTime createdAt,
+        LocalDateTime lastModifiedAt
+) {
 }
