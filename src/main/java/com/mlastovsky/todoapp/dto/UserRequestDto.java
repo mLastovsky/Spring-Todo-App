40a2bbb55692bae9ec08c207f59e64fc73e0a4f2
mLastovsky <LastovskyTech@gmail.com>
2025-04-09 03:30:53 +0300

    create: UserRequestDto

---
+++ src/main/java/com/mlastovsky/todoapp/dto/UserRequestDto.java
@@ -0,0 +1,21 @@
+package com.mlastovsky.todoapp.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserRequestDto(
+
+        @NotBlank(message = "Username cannot be blank")
+        @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+        String username,
+
+        @NotBlank(message = "Email cannot be blank")
+        @Email(message = "Email should be valid")
+        String email,
+
+        @NotBlank(message = "Password cannot be blank")
+        @Size(min = 8, message = "Password must be at least 8 characters long")
+        String password
+) {
+}
