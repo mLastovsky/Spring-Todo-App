28348b7445cc87787fc33c1bfe7ee598cc0767ad
mLastovsky <LastovskyTech@gmail.com>
2025-04-10 00:37:56 +0300

    feat: implement findAllByUserId method

--- src/main/java/com/mlastovsky/todoapp/repository/TodoRepository.java
+++ src/main/java/com/mlastovsky/todoapp/repository/TodoRepository.java
@@ -1,7 +1,14 @@
 package com.mlastovsky.todoapp.repository;
 
 import com.mlastovsky.todoapp.model.Todo;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TodoRepository extends CrudRepository<Todo, Long> {
+
+    @Query("FROM Todo t WHERE t.owner.id = :userId")
+    List<Todo> findAllByUserId(Long userId);
+
 }
