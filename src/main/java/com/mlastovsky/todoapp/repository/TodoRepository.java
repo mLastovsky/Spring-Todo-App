19245f05a63403954c265c056af4d2ef0554f6bc
mLastovsky <LastovskyTech@gmail.com>
2025-04-10 03:12:32 +0300

    feat: update domain repositories, added some methods

--- src/main/java/com/mlastovsky/todoapp/repository/TodoRepository.java
+++ src/main/java/com/mlastovsky/todoapp/repository/TodoRepository.java
@@ -1,5 +1,6 @@
 package com.mlastovsky.todoapp.repository;
 
+import com.mlastovsky.todoapp.dto.TodoResponseDto;
 import com.mlastovsky.todoapp.model.Todo;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -10,5 +11,8 @@ public interface TodoRepository extends CrudRepository<Todo, Long> {
 
     @Query("FROM Todo t WHERE t.owner.id = :userId")
     List<Todo> findAllByUserId(Long userId);
+
+    @Query("FROM Todo t")
+    List<Todo> findAllTodos();
 
 }
--- src/main/java/com/mlastovsky/todoapp/repository/UserRepository.java
+++ src/main/java/com/mlastovsky/todoapp/repository/UserRepository.java
@@ -1,7 +1,18 @@
 package com.mlastovsky.todoapp.repository;
 
 import com.mlastovsky.todoapp.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    @Query("FROM User u")
+    List<User> findAllUsers();
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+
 }
