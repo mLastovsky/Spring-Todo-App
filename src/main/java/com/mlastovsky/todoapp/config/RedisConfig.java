264d15c4e580d01dbcf9d81a9aceac5528c7dee7
mLastovsky <LastovskyTech@gmail.com>
2025-04-14 14:53:19 +0300

    fix: localDateTime serialization problem

--- src/main/java/com/mlastovsky/todoapp/config/RedisConfig.java
+++ src/main/java/com/mlastovsky/todoapp/config/RedisConfig.java
@@ -1,5 +1,11 @@
 package com.mlastovsky.todoapp.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -18,6 +24,19 @@ import java.time.Duration;
 public class RedisConfig {
 
     @Bean
+    public ObjectMapper objectMapper() {
+        return JsonMapper.builder()
+                .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS, true)
+                .configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE, false)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .configure(DeserializationFeature.READ_ENUMS_USING_TO_STRING, true)
+                .addModule(new JavaTimeModule())
+                .findAndAddModules()
+                .build();
+    }
+
+    @Bean
     public CacheManager cacheManager(
             RedisConnectionFactory redisConnectionFactory
     ) {
@@ -31,12 +50,13 @@ public class RedisConfig {
                 )
                 .serializeValuesWith(
                         RedisSerializationContext.SerializationPair
-                                .fromSerializer(new GenericJackson2JsonRedisSerializer())
+                                .fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper()))
                 );
 
         return RedisCacheManager
                 .builder(redisConnectionFactory)
                 .cacheDefaults(config)
+                .transactionAware()
                 .build();
     }
 
